55db72e7fd5253ca270c2fc5b686822bc80b7611
Samuel Richardson <toolbax@gmail.com>
2018-08-14 15:59:43 -0700

    Create area.java

---
+++ CH3/area.java
@@ -0,0 +1,10 @@
+/*
+ *  Write a method named area that accepts the radius of a circle as a parameter and returns the area of a 
+ *  circle with that radius. For example, the call area(2.0) should return 12.566370614359172. 
+ *  You may assume that the radius is non-negative.
+ */
+ 
+public double area(double radius){
+    double result = Math.PI* (radius * radius);
+    return result;
+}
