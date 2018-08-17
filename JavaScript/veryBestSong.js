56c4dd653c82137fc30f6d70d76c63e8356016b6
Samuel Richardson <toolbax@gmail.com>
2018-08-17 10:39:24 -0700

    Create veryBestSong.js

---
+++ JavaScript/veryBestSong.js
@@ -0,0 +1,67 @@
+/*
+ *  Write a function named veryBestSong prints the following song to the console. Use helper functions to show structure and eliminate redundancy in your solution.
+ *  
+ *  I wanna be,
+ *  the very best,
+ *  That no programmer ever was...
+ *  Every function along the way
+ *  With courage I will face...
+ *  JSLint, oh, you're my best friend
+ *  In a World Wide Web we must defend...
+ *  
+ *  Variable scope errors! Gotta try-catch 'em all!
+ *  Those missing semi-colons won't stand a chance,
+ *  I'll win every function call...
+ *  Always debugging my JavaScript code,
+ *  I know it's my destiny...
+ *  
+ *  Bug by bug we'll win the fight
+ *  But we'll leave be the Caterpie...
+ *  Use alerts when the time is right,
+ *  There's no better bug remedy...
+ *  JSLint, oh, you're my best friend
+ *  In a World Wide Web we must defend...
+ *  
+ *  Variable scope errors! Gotta try-catch 'em all!
+ *  Those missing semi-colons won't stand a chance,
+ *  I'll win every function call...
+ *  Always debugging my JavaScript code,
+ *  I know it's my destiny...
+ *  
+ *  I wanna be,
+ *  the very best,
+ *  That no programmer ever was...
+ *  Every function along the way
+ *  With courage I will face...
+ *  JSLint, oh, you're my best friend
+ *  In a World Wide Web we must defend...
+ */
+ 
+ function veryBestSong(){
+    iWanna();
+    variableScope();
+    bugPart();
+    variableScope();
+    iWanna();
+}
+
+function iWanna(){
+    console.log("I wanna be,\nthe very best,\nThat no programmer ever was...");
+    console.log("Every function along the way\nWith courage I will face...");
+    console.log("JSLint, oh, you're my best friend\nIn a World Wide Web we must defend...");
+    console.log();
+}
+
+function variableScope(){
+    console.log("Variable scope errors! Gotta try-catch 'em all!");
+    console.log("Those missing semi-colons won't stand a chance,\nI'll win every function call...");
+    console.log("Always debugging my JavaScript code,\nI know it's my destiny...");
+    console.log();
+}
+
+function bugPart(){
+    console.log("Bug by bug we'll win the fight\nBut we'll leave be the Caterpie...");
+    console.log("Use alerts when the time is right,\nThere's no better bug remedy...");
+    console.log("JSLint, oh, you're my best friend\nIn a World Wide Web we must defend...");
+    console.log();
+}
