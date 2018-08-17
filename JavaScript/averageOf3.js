5db20a71cb40b3a07617ea2f575c90f4289d9344
Samuel Richardson <toolbax@gmail.com>
2018-08-17 12:00:08 -0700

    Create averageOf3.js

---
+++ JavaScript/averageOf3.js
@@ -0,0 +1,9 @@
+/*
+ *  Write a method named averageOf3 that accepts three integers as parameters and returns the 
+ *  average of the three integers as a real number. For example, the call of averageOf3(4, 7, 13) should return 8.
+ */
+ 
+function averageOf3(num1, num2, num3){
+    var result = (num1 + num2 + num3)/ 3;
+    return result;
+}
