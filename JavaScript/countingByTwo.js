ebdc94446a3e70c58f62f075bd9e6eddc4390eb8
Samuel Richardson <toolbax@gmail.com>
2018-08-20 13:34:30 -0700

    Create countingByTwo.js

---
+++ JavaScript/countingByTwo.js
@@ -0,0 +1,14 @@
+/*
+ *  Complete the following code, replacing the "FINISH ME" parts with your own code, to produce the following output:
+ *  
+ *  2 times 1 = 2
+ *  2 times 2 = 4
+ *  2 times 3 = 6
+ *  2 times 4 = 8
+ */
+ 
+function countingByTwo() {
+    for (var i = 1; i <= 4; i++) {
+        console.log("2 times " + i + " = " + (i * 2));
+    }
+}
