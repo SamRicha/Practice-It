4787ddd7d20d2d9c47446d00fbdcdc70fac715a5
Samuel Richardson <toolbax@gmail.com>
2018-08-29 10:51:10 -0700

    Create medianOf3.java

---
+++ CH4/medianOf3.java
@@ -0,0 +1,16 @@
+/*
+ *  The following method attempts to return the median (middle) of three integer values, but it contains 
+ *  logic errors. In what cases does the method return an incorrect result? How can the code be fixed? 
+ *  Determine what is wrong with the code, and submit a corrected version that works properly.
+ */
+
+public static int medianOf3(int n1, int n2, int n3) {
+    if (n1 <= n2 && n1 >= n3 || n1 >= n2 && n1 <= n3) {
+        return n1;
+    }else if(n2 <= n1 && n2 >= n3 || n2 >= n1 && n2 <= n3){
+        return n2;
+    }else if(n3 <= n1 && n3 >= n2 || n3 >= n1 && n1 <= n3){
+        return n3;
+    }
+    return 0;
+}
