ac2b6ebe98e9cb70a05c06c4b5c1945cd685727a
Samuel Richardson <toolbax@gmail.com>
2018-09-15 13:34:52 -0700

    Create PrintMyself.java

---
+++ CH6/PrintMyself.java
@@ -0,0 +1,14 @@
+/*
+ *  Write a complete Java program that prints itself to the console as output. Assume that the program is 
+ *  stored in a file named PrintMyself.java, and make your code open the file PrintMyself.java and print its contents to the console.
+ */
+ 
+public class PrintMyself{
+    public static void main(String[] args) throws FileNotFoundException{
+        Scanner scan = new Scanner(new File("PrintMyself.java"));
+        while(scan.hasNextLine()){
+            String line = scan.nextLine();
+            System.out.println(line);
+        }
+    }
+}
