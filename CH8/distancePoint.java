177e67be99502e30fc395e53378bf2f5472f07b3
Samuel Richardson <toolbax@gmail.com>
2018-10-11 13:35:11 -0700

    Create distancePoint.java

---
+++ CH8/distancePoint.java
@@ -0,0 +1,21 @@
+/*
+ *  Add the following method to the Point class:
+ *  public double distance(Point other)
+ *  
+ *  Returns the distance between the current Point object and the given other Point object. The distance between 
+ *  two points is equal to the square root of the sum of the squares of the differences of their x- and y-coordinates. 
+ *  In other words, the distance between two points (x1, y1) and (x2, y2) can be expressed as the square root of 
+ *  (x2 - x1)2 + (y2 - y1)2. Two points with the same (x, y) coordinates should return a distance of 0.0.
+ *  
+ *  public class Point {
+ *      int x;
+ *      int y;    
+ *      // // your code goes here  
+ *  }
+ */
+ 
+public double distance(Point other){
+    double q = other.x-this.x;
+    double w = other.y-this.y;
+    return Math.sqrt(Math.pow(q, 2)+Math.pow(w, 2));
+}
